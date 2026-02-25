87ce07888f739788b9c6073f549a99df5e6f60d6
Adem Saidi <adem.saidi@esprit.tn>
2026-02-25 20:21:35 +0000

    Update menu.h

--- SovgardMenu/menu.h
+++ SovgardMenu/menu.h
@@ -5,7 +5,7 @@
 #include <SDL2/SDL_image.h>
 #include "button.h"
 
-void updateLayout(SDL_Renderer *renderer, Button *btnOui, Button *btnNon);
+void updateLayout(SDL_Renderer *renderer, Button *btnOui, Button *btnNon,Button *btnLoad,Button *btnNew, SDL_Rect *recTxt);
 void runSaveMenu(SDL_Renderer *renderer);
 
 #endif
